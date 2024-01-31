c8f86eb626964ccf06b2c533fd35614a3e078989
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-02-01 00:49:01 +0200

    TODO:FIX FRONTEND

--- django_tailwind_todo/django_tailwind_todo/todo/urls.py
+++ django_tailwind_todo/django_tailwind_todo/todo/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = (
+from django.urls import path
 
+from django_tailwind_todo.todo.views import index
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('add_todo/', index, name='add_todo'),
+    path('update_todo/<int:pk>/', index, name='update_todo'),
+    # path('delete_todo/<int:pk>/', index, name='delete_todo'),
 )
--- django_tailwind_todo/django_tailwind_todo/todo/views.py
+++ django_tailwind_todo/django_tailwind_todo/todo/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from django_tailwind_todo.todo.models import Todo
+
 
 # Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+
+    return render(
+        request,
+        'todo/index.html',
+        context
+    )
+@require_http_methods(['POST'])
+def add_todo(request):
+    todo = None
+    title = request.POST.get('title', '')
+
+    if title:
+        todo = Todo.objects.create(title=title)
+
+    context = {
+        'todo': todo
+    }
+    return render(
+        request,
+        'partials/todo.html',
+        context
+    )
+@require_http_methods(['PUT'])
+def update_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    title = request.POST.get('title', '')
+
+    todo.is_done = True
+    todo.save()
+
+    context = {
+        'todo': todo
+    }
+    return render(
+        request,
+        'partials/todo.html',
+        context)
